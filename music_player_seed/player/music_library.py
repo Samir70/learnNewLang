ce690dc6510b17828675e887a15c24d79b54dc33
Samir70 <sbetmouni@talktalk.net>
2022-12-12 17:43:30 +0000

    mLib.add(album)

--- music_player_seed/player/music_library.py
+++ music_player_seed/player/music_library.py
@@ -1,2 +1,9 @@
 class MusicLibrary:
-    pass
+    def __init__(self):
+        self.music_list = []
+
+    def all(self):
+        return self.music_list
+
+    def add(self, title):
+        self.music_list.append(title)
--- music_player_seed/tests/test_music_library.py
+++ music_player_seed/tests/test_music_library.py
@@ -6,3 +6,12 @@ from player.music_library import MusicLibrary
 class TestMusicLibrary(unittest.TestCase):
     def test_constructs(self):
         MusicLibrary()
+
+    def test_lists_all(self):
+        ml = MusicLibrary()
+        self.assertEqual(ml.all(), [])
+
+    def test_adds_an_album(self):
+        ml = MusicLibrary()
+        ml.add("Seventh Son of a Seventh Son")
+        self.assertEqual(ml.all(), ["Seventh Son of a Seventh Son"])

f09f37c91d043353c555a97a009a3219826a0079
Samir70 <sbetmouni@talktalk.net>
2022-12-14 15:13:46 +0000

    add mp2Loader.get_files

--- music_player_seed/player/mp3_loader.py
+++ music_player_seed/player/mp3_loader.py
@@ -2,3 +2,8 @@ class Mp3Loader:
     def __init__(self, path, tag_reader, file_sys) -> None:
         self.files = []
         self.path = file_sys.join(*path)
+        self.file_sys = file_sys
+
+    def get_files(self):
+        full_path = self.file_sys.join(self.file_sys.getcwd(), self.path)
+        self.files = [f for f in self.file_sys.listdir(full_path) if f.endswith(".mp3")]
--- music_player_seed/tests/test_mp3_loader.py
+++ music_player_seed/tests/test_mp3_loader.py
@@ -10,4 +10,17 @@ class TestMp3Loader(unittest.TestCase):
         # file_sys.join.assert_called_with("dummy", "path")
         self.assertEqual(mLoad.path, "dummy/path")
 
-    
+    def test_finds_files_in_folder(self):
+        file_sys = Mock()
+        file_sys.getcwd = MagicMock(return_value = "/Users/project/mLib")
+        file_sys.join = MagicMock(return_value = "/Users/project/mLib/dummy/path")
+        file_sys.is_file.side_effect = [True, False, True, True] 
+        file_sys.listdir = MagicMock(return_value = ["file1.mp3", "subdir/", "file2.mp3", "file3.txt"])
+        mLoad = Mp3Loader("dummy/path", None, file_sys)
+        mLoad.get_files()
+        file_sys.getcwd.assert_called
+        file_sys.join.assert_called
+        file_sys.is_file.assert_called
+        file_sys.listdir.assert_called_with("/Users/project/mLib/dummy/path")
+        self.assertEqual(mLoad.files, ["file1.mp3", "file2.mp3"])
+

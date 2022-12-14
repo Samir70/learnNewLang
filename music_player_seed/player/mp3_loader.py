9a46a9c261e64eff328af98284f2a1a1eec4377a
Samir70 <sbetmouni@talktalk.net>
2022-12-14 16:16:42 +0000

    mp3Loader.get_files loads data if given tag_reader

--- music_player_seed/player/mp3_loader.py
+++ music_player_seed/player/mp3_loader.py
@@ -1,9 +1,21 @@
 class Mp3Loader:
     def __init__(self, path, tag_reader, file_sys) -> None:
-        self.files = []
+        self.file_names = []
+        self.file_data = []
         self.path = file_sys.join(*path)
         self.file_sys = file_sys
+        self.tag_reader = tag_reader
 
     def get_files(self):
         full_path = self.file_sys.join(self.file_sys.getcwd(), self.path)
-        self.files = [f for f in self.file_sys.listdir(full_path) if f.endswith(".mp3")]
+        self.file_names = [f for f in self.file_sys.listdir(full_path) if f.endswith(".mp3")]
+        if self.tag_reader == None:
+            return
+        for f in self.file_names:
+            audio = self.tag_reader.load(self.file_sys.join(full_path, f))
+            # print(audio["tag"])#.tag.title)
+            self.file_data.append({
+                "title": audio["tag"]["title"], 
+                "artist": audio["tag"]["artist"],
+                "file": self.file_sys.join(self.path, f)
+            })
--- music_player_seed/tests/test_mp3_loader.py
+++ music_player_seed/tests/test_mp3_loader.py
@@ -2,25 +2,52 @@ import unittest
 from unittest.mock import Mock, MagicMock
 from player.mp3_loader import Mp3Loader
 
+
 class TestMp3Loader(unittest.TestCase):
     def test_constructs(self):
         file_sys = Mock()
-        file_sys.join = MagicMock(return_value = "dummy/path")
+        file_sys.join = MagicMock(return_value="dummy/path")
         mLoad = Mp3Loader(["dummy", "path"], None, file_sys)
-        # file_sys.join.assert_called_with("dummy", "path")
+        file_sys.join.assert_called_with("dummy", "path")
         self.assertEqual(mLoad.path, "dummy/path")
 
     def test_finds_files_in_folder(self):
         file_sys = Mock()
-        file_sys.getcwd = MagicMock(return_value = "/Users/project/mLib")
-        file_sys.join = MagicMock(return_value = "/Users/project/mLib/dummy/path")
-        file_sys.is_file.side_effect = [True, False, True, True] 
-        file_sys.listdir = MagicMock(return_value = ["file1.mp3", "subdir/", "file2.mp3", "file3.txt"])
-        mLoad = Mp3Loader("dummy/path", None, file_sys)
+        file_sys.getcwd = MagicMock(return_value="/Users/project/mLib")
+        file_sys.join = MagicMock(return_value="/Users/project/mLib/dummy/path")
+        file_sys.is_file.side_effect = [True, False, True, True]
+        file_sys.listdir = MagicMock(
+            return_value=["file1.mp3", "subdir/", "file2.mp3", "file3.txt"])
+        mLoad = Mp3Loader(["dummy", "path"], None, file_sys)
         mLoad.get_files()
         file_sys.getcwd.assert_called
         file_sys.join.assert_called
         file_sys.is_file.assert_called
         file_sys.listdir.assert_called_with("/Users/project/mLib/dummy/path")
-        self.assertEqual(mLoad.files, ["file1.mp3", "file2.mp3"])
+        self.assertEqual(mLoad.file_names, ["file1.mp3", "file2.mp3"])
 
+    def test_gets_file_data(self):
+        file_sys = Mock()
+        file_sys.listdir = MagicMock(
+            return_value=["file1.mp3", "subdir/", "file2.mp3", "file3.txt"])
+        file_sys.is_file.side_effect = [True, False, True, True]
+        file_sys.join.side_effect = [
+            "used in __init__/",
+            "should be from getcwd/",
+            "to access first file/",
+            "dummy/path/file1.mp3",
+            "to access second file/",
+            "dummy/path/file2.mp3"
+        ]
+        fake_eyed3 = Mock()
+        fake_eyed3.load.side_effect = [
+            {"tag": {"title": "title1", "artist": "artist1"}},
+            {"tag": {"title": "title2", "artist": "artist2"}},
+        ]
+        mLoad = Mp3Loader(["dummy", "path"], fake_eyed3, file_sys)
+        mLoad.get_files()
+        fake_eyed3.load.assert_called
+        self.assertEqual(mLoad.file_data, [
+            {"title": "title1", "artist": "artist1", "file": "dummy/path/file1.mp3"},
+            {"title": "title2", "artist": "artist2", "file": "dummy/path/file2.mp3"}
+        ])

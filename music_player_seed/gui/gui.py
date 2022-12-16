daaee43ea6b49fa6854703032b338b602d45753b
Samir70 <sbetmouni@talktalk.net>
2022-12-16 16:11:04 +0000

    enble select and play of music thru tk gui

--- music_player_seed/gui/gui.py
+++ music_player_seed/gui/gui.py
@@ -8,6 +8,9 @@ class GUI:
         self.music_library = MusicLibrary()
         self.music_player = MusicPlayer(subprocess)
         self.mp3_loader = Mp3Loader(["data", "mp3s"], tag_reader, file_sys)
+        self.labels = { "artists": {}, "titles": {}}
+        self.selected_artist = ""
+        self.selected_track = ""
 
     def run(self):
         self.mp3_loader.get_files()
@@ -22,35 +25,70 @@ class GUI:
         self._showGUI()
     
     def _showGUI(self):
-        print("in the show gui method")
-        window = tk.Tk()
-        headings = tk.Frame()
-        headings.pack()
-        headings.rowconfigure(0, minsize=50, weight=1)
-        headings.columnconfigure(0, minsize=50, weight=1)
-        headings.columnconfigure(1, minsize=150, weight=3)
-        lbl_artist = tk.Label(headings, text="Artists")
+        self.window = tk.Tk()
+        play_btn = tk.Button(self.window, text="PLAY", command=self._play_track)
+        play_btn.pack()
+        stop_btn = tk.Button(self.window, text="STOP", command=self._stop_playback)
+        stop_btn.pack()
+        self.headings = tk.Frame()
+        self.headings.pack()
+        self.headings.rowconfigure(0, minsize=50, weight=1)
+        self.headings.columnconfigure(0, minsize=50, weight=1)
+        self.headings.columnconfigure(1, minsize=300, weight=3)
+        lbl_artist = tk.Label(self.headings, text="Artists")
         lbl_artist.grid(row=0, column=0)
-        lbl_title = tk.Label(headings, text="Track title")
+        lbl_title = tk.Label(self.headings, text="Track title")
         lbl_title.grid(row=0, column=1)
 
         tracks = self.music_library.all()
         artists = sorted([" All", *list(set([t.artist for t in tracks]))])
         r = 1
         for artist in artists:
-            tk.Label(headings, text=f"{artist}").grid(row=r, column=0)
+            temp_lbl = tk.Label(self.headings, text=f"{artist}")
+            temp_lbl.bind("<Button-1>", self._artist_clicked)
+            temp_lbl.grid(row=r, column=0)
+            self.labels["artists"][artist] = temp_lbl
             r += 1
-        window.mainloop()
+        self._highlight("artists", " All", "#5555FF")
+        self.selected_artist = " All"
+        self._list_tracks(tracks)
+        self.window.mainloop()
 
+    def _artist_clicked(self, event):
+        name = event.widget["text"]
+        self._highlight("artists", self.selected_artist, "systemWindowBackgroundColor")
+        self.selected_artist = name
+        self.selected_track = ""
+        self._highlight("artists", name, "#5555FF")
+        if name == " All":
+            tracks = self.music_library.all()
+        else:
+            tracks = [track for track in self.music_library.all() if track.artist == name]
+        self._list_tracks(tracks)
+
+    def _title_clicked(self, event):
+        name = event.widget["text"]
+        if self.selected_track != "":
+            self._highlight("titles", self.selected_track, "systemWindowBackgroundColor")
+        self._highlight("titles", name, "#5555FF")
+        self.selected_track = name
 
     def _add_track(self, title, artist, file):
         self.music_library.add(Track(title, artist, file))
 
+    def _highlight(self, type, name, colour):
+        self.labels[type][name].configure(bg=colour)
+
     def _list_tracks(self, tracks):
-        for idx, track in enumerate(tracks):
-            self.console.print(
-                f"{idx + 1}. {track}"
-            )
+        for title in self.labels["titles"]:
+            self.labels["titles"][title].destroy()
+        r = 0
+        for track in tracks:
+            r += 1
+            title_lbl = tk.Label(self.headings, text=f"{track.title}")
+            title_lbl.bind("<Button-1>", self._title_clicked)
+            title_lbl.grid(row=r, column= 1)
+            self.labels["titles"][track.title] = title_lbl
 
     def _search_tracks(self):
         self.console.print("Search by:")
@@ -94,24 +132,20 @@ class GUI:
         #     self.console.print("No such field!")
 
     def _play_track(self):
-        tracks = self._search_tracks()
-        # tracks = self.music_library.all()
-        track_id = int(self.console.input("Which do you want to play? ")) - 1
-        if track_id >= 0 and track_id < len(tracks):
-            track = tracks[track_id]
-            t_str = f"{track}".split(" @")[0]
-            self.console.print(f"Playing {t_str}...")
-            self.music_player.play(track.file)
-            self.console.print("Done.")
+        if self.selected_track == "":
+            track_name = list(self.labels["titles"].keys())[0]
         else:
-            self.console.print("No such track.")
+            track_name = self.selected_track
+        print(f"Trying to play{track_name}")
+        tracks = self.music_library.search("title", track_name.lower())
+        print(tracks)
+        if tracks[0] == None:
+            print(f"?????? didn't find a track to play {track_name}")
+            return
+        else:
+            self.music_player.play(tracks[0].file)
 
-    def _remove_track(self):
-        self._list_tracks(self.music_library.all())
-        track_id = int(self.console.input("Which do you want to delete? ")) - 1
-        if self.music_library.remove(track_id):
-            self.console.print("Deleted successfully.")
-        else:
-            self.console.print("No such track.")
+    def _stop_playback(self):
+        pass
 
 
